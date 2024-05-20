86b965a57b8424ed62c61d317713634ad485738c
AviduDev <avidudev@gmail.com>
2024-05-20 13:16:04 +0530

    Revamped

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold tracking-tighter">Home</h1>
-      <Button asChild>
-        <Link href={"/tours"}>Go to Tours</Link>
-      </Button>
+     <Button>This is button</Button>
     </main>
   );
 }
--- app/tours/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Tours() {
-  return (
-    <main>Tours</main>
-  )
-}
